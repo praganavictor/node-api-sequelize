fdfe4cc60c501a2a0624ddb00b60626b9f4d2827
Victor Pragana <victormerigue@gmail.com>
2019-08-26 17:45:47 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: "root",
+  password: "ZXCpragana987",
+  database: "crud_sequelize",
+  host: "127.0.0.1",
+  dialect: "mysql"
+};
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const { User } = require("./app/models");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/register", async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+app.get("/find/:id", (req, res) => {
+  res.json("Im in register");
+});
+
+app.get("/findall", (req, res) => {
+  res.json("Im in Find All");
+});
+
+app.put("/update/:id", (req, res) => {
+  res.json("Im in update");
+});
+
+app.delete("/delete/:id", (req, res) => {
+  res.json("Im in delete");
+});
+
+app.listen(3000);
